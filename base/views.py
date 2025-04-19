8f5655abff61359818f7ce93ee0639648777896f
jyoti chetry <jyotichetry087@gmail.com>
2025-04-19 09:18:52 +0530

    models

--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
+
+rooms =[
+    {'id':1,'name':"Python with me"},
+    {'id':2,'name':"Web development"},
+    {'id':3,'name':"Java"},
+    {'id':4,'name':"Canva"}
+]
 
 def home(request):
-    return render(request,'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request,pk):
+    room  = Room.objects.get(id=pk)
+    context = {'room':room} 
+    
+    return render(request,'base/room.html',context)
 
+    
