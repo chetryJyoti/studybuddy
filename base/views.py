219df5ce2f02245b25e3cb3e9e0fa9a3b55d8585
jyoti chetry <jyotichetry087@gmail.com>
2025-04-25 00:31:30 +0530

    user login/register

--- base/views.py
+++ base/views.py
@@ -3,12 +3,58 @@ from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
-# rooms =[
-#     {'id':1,'name':"Python with me"},
-#     {'id':2,'name':"Web development"},
-#     {'id':3,'name':"Java"},
-#     {'id':4,'name':"Canva"}
-# ]
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            
+        except:
+            messages.error(request,'User does not exit')  
+        
+        user = authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Username or password does not match')
+
+    context = {'page':page}
+    return render(request,'base/login_register.html',context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
+                login(request,user)
+                return redirect('home')
+        else:
+            messages.error(request,'Something went wrong! Try again.')
+        # todo: if the username already exists then show an message saying its already taken
+    return render(request,'base/login_register.html',{'form':form})
+    
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
@@ -27,6 +73,7 @@ def room(request,pk):
     context = {'room':room} 
     return render(request,'base/room.html',context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     
@@ -39,9 +86,13 @@ def createRoom(request):
     context = {'form':form}
     return render(request,'base/room_form.html',context)
 
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse('Your are not the owner of this room!')
     
     if request.method == "POST":
         form  = RoomForm(request.POST,instance=room)
@@ -52,7 +103,7 @@ def updateRoom(request,pk):
     context = {'form':form}
     return render(request,'base/room_form.html',context)
 
-
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk) 
     if request.method =='POST':
